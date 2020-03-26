5a340a4f3b9d44c254ea1ad5f8b0574478815acf
Madhuri Bayyagari <mbayyagari@nisum.com>
2020-03-26 17:14:01 +0530

    GET requset implented
    GET requset implented

---
+++ src/test/java/CreateRequest.java
@@ -0,0 +1,21 @@
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import io.restassured.RestAssured;
+import io.restassured.http.Method;
+import io.restassured.response.Response;
+import io.restassured.specification.RequestSpecification;
+
+public class CreateRequest {
+
+	@Test
+	void getUserDetails(){
+		RestAssured.baseURI = "http://dummy.restapiexample.com/api/v1/employees";
+		RequestSpecification httpRequest = RestAssured.given();
+		Response response = httpRequest.request(Method.GET);
+		String responseBody = response.getBody().asString();
+//		System.out.println(responseBody);
+		int statusCode = response.getStatusCode();
+        Assert.assertEquals(statusCode, 200, statusCode);
+	}
+}
